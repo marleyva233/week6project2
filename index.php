bfd98edf1e2f205557b2dd92182f158bd3a7a650
Maribel <marleyva233@gmail.com>
2019-02-11 11:34:24 -0800

    photo gallery

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!-- meta tag -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Welcome to our wedding website!</title>
+		<!-- font awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+		<!-- bootstrap CDN -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<!-- animate css -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+		<!-- custom fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Prata|Rochester" rel="stylesheet">
+		<!-- my custom css -->
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+
+
+
+	<body class="p-0 m-0">
+		<div id="home">
+		</div>
+
+				<!-- STARTING NAVIGATION -->
+		<nav id="nav" class="bg-plum p-0">
+			<ul class="bg-white nav justify-content-center">
+			  <li class="nav-item animated fadeIn">
+			    <a class="nav-link active" href="#home">About Us</a>
+			  </li>
+			  <li class="nav-item animated fadeIn">
+			    <a class="nav-link" href="#ceremony">Ceremony</a>
+			  </li>
+			  <li>
+			  	<a id="meetlink" class="nav-link" href="#meet">Meet Our Wedding Party</a>
+			  </li>
+			  <li class="nav-item animated fadeIn">
+			    <a id="photolink" class="nav-link" href="#photo">Photo Gallery</a>
+			  </li>
+			  <li class="nav-item animated fadeIn">
+			  	<a id="registry" class="nav-link" href="https://www.target.com/gift-registry/gift/dc6d134a6c6e4ab3859f1c46683addf4" target="_blank">Gift Registry</a>
+			  </li>
+			</ul>
+		</nav>
+		<!-- ENDING THE NAVIGATION -->
+
+
+
+
+
+
+		<!-- trying to add picture background here -->
+		<div class="bg-pic container-fluid p-0 m-0">
+			<img src="img/1.jpg" alt="a couple hugging" class="img-fluid m-0">
+			<div class="announcement container-fluid">
+				<h1 class="text-light animated fadeIn mx-auto container-fluid">Dimitri Gonzales and Jessica Ruiz are getting married!
+				</h1>
+				<h1 id="demo">
+				</h1>
+			</div>
+		</div>
+
+
+
+	
+
+		<!-- STARTING MAIN CONTENT -->
+		<!-- ABOUT US/HOME PAGE -->
+		<div id="content" class="container-fluid px-3">
+			<div class="row pt-4">
+				<div class="pt-5 col-12 mx-auto">
+					<h1 class="text-center animated fadeIn">Welcome!</h1>
+				</div>
+				<div class="col-md-10 col-sm-12 mx-auto">
+					<p>Dimitri:
+						"well this is a story all bout how my life got flipped turned upside down..."
+					</p>
+					<p>Jessica:
+						*record scratch* *freeze frame* Yup, that's me. You're probably wondering how I ended up in this situation. Well let me tell you, it's been an amazing journey. It all started at Safeway. I was working a miserable job and I felt miserable. Then I met Dimitri, who worked in the miserable job with me. The moment we started talking it was like everything went from wrong to right. And the stars came out and filled up the sky. The music you were playing really blew my mind. It was love at first sight. By Kylie Minogue. But for real, never knew someone like him could exist. I'm so f*****g happy.
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- ENDING MAIN SECTION -->
+
+		<!-- STARTING CEREMONY SECTION -->
+		<section id="ceremony">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12 mx-auto p-2 bg-plum">
+						<h1>Coming to a Theather Near YOU!</h1>
+						<img src="http://jessicadimitri.info/wp-content/uploads/2018/08/poster.png" alt="movie poster">
+						<p>December 01, 2018 </p>
+						<h4>three in the afternoon</h4>
+						<p>Coyote Grand Hall</p>
+						<p>8140 Monterey Road, Coyote, California<br>
+						[exit at Bailey Avenue and follow the purple balloons!]
+						</p>
+						<p>
+							<a href="https://goo.gl/maps/KNUj8M32eY52" target="_blank" class="text-light">Directions</a>
+						</p>
+						<p>The ceremony will be followed by food and drinks. Please RSVP (by Nov 20) as seating will be strictly based off of RSVP submissions. See you there</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ENDING CEREMONY SECTION -->
+
+		<!-- STARTING OTHER SECTION -->
+<!-- 		<section id="meet" class="pt-5">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<h1>Meet our wedding party</h1>
+					
+					</div>
+				</div>
+
+				<div class="row pt-3">
+					<div class="col-12">
+						<h3>The Guys:</h3>
+					</div>
+				</div>
+
+				<div class="row pb-3">
+					<div class="col-3 mx-auto">
+						<h4>Eddie Aguilar</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+
+					<div class="col-3 mx-auto">
+						<h4>Joey Carrasco</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+			
+					<div class="col-3 mx-auto">
+						<h4>LJ Warf</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+				</div>
+
+				<div class="row pt-5">
+					<div class="col-12">
+						<h3>The Girls:</h3>
+					</div>
+				</div>
+
+				<div class="row pb-3">
+					<div class="col-3 mx-auto">
+						<h4>Laura Boyzo</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+
+					<div class="col-3 mx-auto">
+						<h4>Elias Gonzalez</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+				
+					<div class="col-3 mx-auto">
+						<h4>Ana Martinez</h4>
+						<img src="img/partyportrait.jpg" class="img-fluid party-portrait">
+					</div>
+				</div>
+
+			</div>
+		</section>
+ -->		<!-- ENDING OTHER SECTION -->
+
+
+		<!-- STARTING PHOTO GALLERY SECTION -->
+		<section id="photo" class="bg-plum">
+			<div class="container-fluid bg-plum">
+				<div class="row">
+					<div class="col-12 pt-5 mx-auto bg-plum">
+						<h1>Photo Gallery</h1>
+						<!-- CAROUSEL CODE -->
+						<div id="carouselExampleSlidesOnly" class="carousel slide bg-plum" data-ride="carousel">
+						  <div class="carousel-inner">
+						    <div class="carousel-item active bg-plum">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/1-1024x682.jpg" alt="First slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/6-1024x682.jpg" alt="Second slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/2-1024x682.jpg" alt="Third slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/2-1024x682.jpg" alt="fourth slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/4-768x1154.jpg" alt="Fifth slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/3-1024x682.jpg" alt="Sixth slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100" src="http://jessicadimitri.info/wp-content/uploads/2018/08/5-768x1154.jpg" alt="Seventh slide">
+						    </div>
+						  </div>
+						</div>
+						<!-- END CAROUSEL CODE -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- ENDING PHOTO GALLERY SECTION -->
+
+
+
+
+
+		<!--jquery -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<!-- popper is required for bootstrap 4 js functionality -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<!-- makes bootstrap 4 js work -->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		<!-- my custom js -->
+		<script type="text/javascript" src="js/main.js"></script>
+	</body>
+</html>
+
+
+
+
+
+
+
+
